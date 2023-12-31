8d45d7d3095de3c7202c3026d311588d91f4c434
Ridho Prasetya <ndiing.inc@gmail.com>
2024-01-01 01:49:23 +0700

    clean code at router

--- src/lib/router.js
+++ src/lib/router.js
@@ -7,6 +7,7 @@
 //     redirect:'/login',
 //     title:'Welcome'
 // }
+// const routes=[route]
 
 /**
  * A simple router implementation for managing routes.
@@ -26,8 +27,8 @@ class Router {
      */
 
     /**
-     * Represents an array of route configuration objects.
-     * @type {Route<Array>}
+     * Array containing route configurations.
+     * @type {Route[]}
      */
 
     /**
@@ -314,39 +315,39 @@ class Router {
      * Initialize the router with provided routes.
      * @param {Route<Array>} routes - Array of route objects.
      * @example
-* import { Router } from "./lib/router.js";
-* 
-* import AppMain from "./dev/main.js";
-* import AppUsers from "./dev/users.js";
-* import AppUser from "./dev/user.js";
-* import AppError from "./dev/error.js";
-* 
-* const beforeLoad = async (resolve, reject) => {
-*     if (localStorage.isAuthenticated==="1") resolve()
-*     else {
-*         Router.navigate("/login");
-*         reject();
-*     }
-* };
-* 
-* // prettier-ignore
-* Router.init([
-*     {path:'',title:'Welcome',component:AppMain,children:[
-*         {path:'users',beforeLoad,title:'Users',component:AppUsers,children:[
-*             {path:':_id',title:'User',component:AppUser,children:[]},
-*         ]},
-*         {path:'blogs',title:'Blogs',load:() => import("./dev/blogs.js").then(m=>m.default),children:[
-*             {path:':_id',title:'Blog',load:() => import("./dev/blog.js").then(m=>m.default),children:[]},
-*         ]},
-*     ]},
-*     {path:'/login',title:'Login',load:() => import("./dev/login.js").then(m=>m.default),children:[]},
-*     {path:'*',title:'Error',component:AppError,children:[]},
-* ]);
-* 
-* // window.addEventListener("onNavigationStart", console.log);
-* // window.addEventListener("onNavigation", console.log);
-* // window.addEventListener("onNavigationCancel", console.log);
-* // window.addEventListener("onNavigationEnd", console.log);
+     * import { Router } from "./lib/router.js";
+     *
+     * import AppMain from "./dev/main.js";
+     * import AppUsers from "./dev/users.js";
+     * import AppUser from "./dev/user.js";
+     * import AppError from "./dev/error.js";
+     *
+     * const beforeLoad = async (resolve, reject) => {
+     *     if (localStorage.isAuthenticated==="1") resolve()
+     *     else {
+     *         Router.navigate("/login");
+     *         reject();
+     *     }
+     * };
+     *
+     * // prettier-ignore
+     * Router.init([
+     *     {path:'',title:'Welcome',component:AppMain,children:[
+     *         {path:'users',beforeLoad,title:'Users',component:AppUsers,children:[
+     *             {path:':_id',title:'User',component:AppUser,children:[]},
+     *         ]},
+     *         {path:'blogs',title:'Blogs',load:() => import("./dev/blogs.js").then(m=>m.default),children:[
+     *             {path:':_id',title:'Blog',load:() => import("./dev/blog.js").then(m=>m.default),children:[]},
+     *         ]},
+     *     ]},
+     *     {path:'/login',title:'Login',load:() => import("./dev/login.js").then(m=>m.default),children:[]},
+     *     {path:'*',title:'Error',component:AppError,children:[]},
+     * ]);
+     *
+     * // window.addEventListener("onNavigationStart", console.log);
+     * // window.addEventListener("onNavigation", console.log);
+     * // window.addEventListener("onNavigationCancel", console.log);
+     * // window.addEventListener("onNavigationEnd", console.log);
      */
     static init(routes = []) {
         // Add the provided routes to the router
