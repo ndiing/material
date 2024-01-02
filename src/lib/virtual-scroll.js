199cc06e788606eb437076f2513587e8727fb5da
Ridho Prasetya <ndiing.inc@gmail.com>
2024-01-02 08:14:04 +0700

    save

--- src/lib/virtual-scroll.js
+++ src/lib/virtual-scroll.js
@@ -1,4 +1,4 @@
-import { Library } from './library';
+import { Library } from "./library";
 
 /**
  * Class representing a virtual scroll.
@@ -6,59 +6,62 @@ import { Library } from './library';
  * @author Ridho Prasetya
  */
 class VirtualScroll extends Library {
-  /**
-   * Initializes the virtual scroll.
-   */
-  init() {
-    this.handleScroll = this.handleScroll.bind(this);
-    this.on('scroll', this.handleScroll);
-  }
-
-  /**
-   * Destroys the virtual scroll.
-   */
-  destroy() {
-    this.off('scroll', this.handleScroll);
-  }
-
-  /**
-   * Handles the scroll event.
-   * @param {Event} event - The scroll event.
-   * @fires root#onScroll
-   */
-  handleScroll(event) {
-    const total = this.options.total; // Total number of items
-    const contentHeight = this.options.contentHeight; // Height of each item
-    const scrollTop = this.root.scrollTop; // Scroll position
-    const threshold = this.options.threshold; // Threshold value
-    const viewportHeight = this.root.clientHeight; // Height of the viewport
-
-    const scrollbarHeight = total * contentHeight;
-
-    let start = Math.floor(scrollTop / contentHeight) - threshold;
-    start = Math.max(0, start);
-
-    let limit = Math.ceil(viewportHeight / contentHeight) + 2 * threshold;
-    limit = Math.min(total - start, limit);
-
-    const translateY = start * contentHeight;
+    /**
+     * Initializes the virtual scroll.
+     */
+    init() {
+        this.handleScroll = this.handleScroll.bind(this);
+        this.on("scroll", this.handleScroll);
+    }
 
     /**
-     * Scroll event data.
-     * @typedef {Object} ScrollEventData
-     * @property {number} scrollbarHeight - Height of the scrollbar.
-     * @property {number} start - Starting index of the visible items.
-     * @property {number} limit - Number of items visible in the viewport.
-     * @property {number} translateY - Translation value based on scroll.
+     * Destroys the virtual scroll.
      */
+    destroy() {
+        this.off("scroll", this.handleScroll);
+    }
 
-    this.emit('onScroll', /** @type {ScrollEventData} */ ({
-      scrollbarHeight,
-      start,
-      limit,
-      translateY,
-    }));
-  }
+    /**
+     * Handles the scroll event.
+     * @param {Event} event - The scroll event.
+     * @fires root#onScroll
+     */
+    handleScroll(event) {
+        const total = this.options.total; // Total number of items
+        const contentHeight = this.options.contentHeight; // Height of each item
+        const scrollTop = this.root.scrollTop; // Scroll position
+        const threshold = this.options.threshold; // Threshold value
+        const viewportHeight = this.root.clientHeight; // Height of the viewport
+
+        const scrollbarHeight = total * contentHeight;
+
+        let start = Math.floor(scrollTop / contentHeight) - threshold;
+        start = Math.max(0, start);
+
+        let limit = Math.ceil(viewportHeight / contentHeight) + 2 * threshold;
+        limit = Math.min(total - start, limit);
+
+        const translateY = start * contentHeight;
+
+        /**
+         * Scroll event data.
+         * @typedef {Object} ScrollEventData
+         * @property {number} scrollbarHeight - Height of the scrollbar.
+         * @property {number} start - Starting index of the visible items.
+         * @property {number} limit - Number of items visible in the viewport.
+         * @property {number} translateY - Translation value based on scroll.
+         */
+
+        this.emit(
+            "onScroll",
+            /** @type {ScrollEventData} */ ({
+                scrollbarHeight,
+                start,
+                limit,
+                translateY,
+            })
+        );
+    }
 }
 
 export { VirtualScroll };
