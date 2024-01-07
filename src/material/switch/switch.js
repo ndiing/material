e29e80854be4eacae986306b3ef4c2f76d87367b
Ridho Prasetya <ndiing.inc@gmail.com>
2024-01-07 11:38:40 +0700

    fix

--- src/material/switch/switch.js
+++ src/material/switch/switch.js
@@ -1,5 +1,5 @@
 import { html } from "lit";
-import { ifDefined } from "lit/directives/if-defined";
+import { ifDefined } from "lit/directives/if-defined.js";
 import { MDComponent } from "../foundation/component";
 import { MDRipple } from "../foundation/ripple";
 
