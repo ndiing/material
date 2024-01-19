88dee18c095704caf9298d455746f5e2c7f35a20
Ridho Prasetya <ndiing.inc@gmail.com>
2024-01-20 02:46:09 +0700

    fixed `emoji`

--- src/material/emoji/emoji.js
+++ src/material/emoji/emoji.js
@@ -1,91 +1,89 @@
-import { LitElement, html, nothing } from 'lit';
-import { MdStateController } from '../state/state';
+import { LitElement, html, nothing } from "lit";
+import { MdStateController } from "../state/state";
 
 /**
  * Represents the MdEmojiComponent custom element.
  * @extends LitElement
  */
 class MdEmojiComponent extends LitElement {
-  /**
-   * The properties for MdEmojiComponent.
-   * @returns {Object} The properties object.
-   * @property {Object} properties - The properties object.
-   * @property {string} properties.exampleProperty - An example property.
-   * @static
-   */
-  static get properties() {
-    return {
-      // Define your properties here, if any.
-    };
-  }
+    /**
+     * The properties for MdEmojiComponent.
+     * @returns {Object} The properties object.
+     * @property {Object} properties - The properties object.
+     * @property {string} properties.exampleProperty - An example property.
+     * @static
+     */
+    static get properties() {
+        return {
+            // Define your properties here, if any.
+        };
+    }
 
-  /**
-   * Constructs an instance of MdEmojiComponent.
-   * @constructor
-   */
-  constructor() {
-    super();
-  }
+    /**
+     * Constructs an instance of MdEmojiComponent.
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
 
-  /**
-   * Overrides the default render root to be the component itself.
-   * @returns {this} The rendered root.
-   * @override
-   */
-  createRenderRoot() {
-    return this;
-  }
+    /**
+     * Overrides the default render root to be the component itself.
+     * @returns {this} The rendered root.
+     * @override
+     */
+    createRenderRoot() {
+        return this;
+    }
 
-  /**
-   * Adds the "md-emoji" class when the component is connected.
-   * @override
-   */
-  connectedCallback() {
-    super.connectedCallback();
-    this.classList.add('md-emoji');
-  }
+    /**
+     * Adds the "md-emoji" class when the component is connected.
+     * @override
+     */
+    connectedCallback() {
+        super.connectedCallback();
+        this.classList.add("md-emoji");
+    }
 
-  /**
-   * Removes the "md-emoji" class when the component is disconnected.
-   * @override
-   */
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this.classList.remove('md-emoji');
-  }
+    /**
+     * Removes the "md-emoji" class when the component is disconnected.
+     * @override
+     */
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        this.classList.remove("md-emoji");
+    }
 
-  /**
-   * Lifecycle callback invoked once when the element is first updated.
-   * @override
-   */
-  firstUpdated() {
-    // Add your logic here.
-  }
+    /**
+     * Lifecycle callback invoked once when the element is first updated.
+     * @override
+     */
+    firstUpdated() {
+        // Add your logic here.
+    }
 
-  /**
-   * Lifecycle callback invoked whenever the element is updated.
-   * @param {Map} _changedProperties - The changed properties.
-   * @override
-   */
-  updated(_changedProperties) {
-    // Add your logic here.
-  }
+    /**
+     * Lifecycle callback invoked whenever the element is updated.
+     * @param {Map} _changedProperties - The changed properties.
+     * @override
+     */
+    updated(_changedProperties) {
+        // Add your logic here.
+    }
 
-  /**
-   * Dispatches a custom event.
-   * @fires MdEmojiComponent#customEvent
-   */
-  dispatchCustomEvent() {
-    this.dispatchEvent(
-      new CustomEvent('customEvent', { detail: 'Event detail' })
-    );
-  }
+    /**
+     * Dispatches a custom event.
+     * @fires MdEmojiComponent#customEvent
+     */
+    dispatchCustomEvent() {
+        this.dispatchEvent(new CustomEvent("customEvent", { detail: "Event detail" }));
+    }
 }
 
 /**
  * A custom element for displaying emoji.
  * @typedef {MdEmojiComponent} md-emoji
  */
-customElements.define('md-emoji', MdEmojiComponent);
+customElements.define("md-emoji", MdEmojiComponent);
 
 export { MdEmojiComponent };
