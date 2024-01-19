e35673a743e15cf39bfff60354abcc6b4789c925
Ridho Prasetya <ndiing.inc@gmail.com>
2024-01-20 02:47:43 +0700

    fixed `fab`

--- src/material/fab/fab.js
+++ src/material/fab/fab.js
@@ -1,5 +1,5 @@
-import { LitElement, html, nothing } from 'lit';
-import { MdStateController } from '../state/state';
+import { LitElement, html, nothing } from "lit";
+import { MdStateController } from "../state/state";
 
 /**
  * Custom element for a Material Design Floating Action Button (FAB).
@@ -7,98 +7,98 @@ import { MdStateController } from '../state/state';
  * @extends LitElement
  */
 class MdFabComponent extends LitElement {
-  /**
-   * @property {String} type - The type of the button.
-   * @property {String} label - The label text for the FAB.
-   * @property {String} icon - The icon for the FAB.
-   * @property {String} size - The size of the FAB. Can be "small" or "large".
-   * @property {Boolean} extended - Indicates whether the FAB is in extended mode.
-   */
-  static get properties() {
-    return {
-      type: { type: String },
-      label: { type: String },
-      icon: { type: String },
-      size: { type: String },
-      extended: { type: Boolean },
-    };
-  }
+    /**
+     * @property {String} type - The type of the button.
+     * @property {String} label - The label text for the FAB.
+     * @property {String} icon - The icon for the FAB.
+     * @property {String} size - The size of the FAB. Can be "small" or "large".
+     * @property {Boolean} extended - Indicates whether the FAB is in extended mode.
+     */
+    static get properties() {
+        return {
+            type: { type: String },
+            label: { type: String },
+            icon: { type: String },
+            size: { type: String },
+            extended: { type: Boolean },
+        };
+    }
 
-  constructor() {
-    super();
-    this.type = 'button';
-  }
+    constructor() {
+        super();
+        this.type = "button";
+    }
 
-  createRenderRoot() {
-    return this;
-  }
+    createRenderRoot() {
+        return this;
+    }
 
-  render() {
-    // prettier-ignore
-    return html`
+    render() {
+        // prettier-ignore
+        return html`
             <button class="md-fab__native"
                 .type="${this.type}"
             ></button>
             ${this.icon?html`<div class="md-fab__icon">${this.icon}</div>`:nothing}
             ${this.label?html`<div class="md-fab__label">${this.label}</div>`:nothing}
         `;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.classList.add('md-fab');
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this.classList.remove('md-fab');
-  }
-
-  /**
-   * Returns the native button element of the FAB.
-   *
-   * @readonly
-   * @returns {HTMLButtonElement} The native button element.
-   */
-  get fabNative() {
-    return this.querySelector('.md-fab__native');
-  }
-
-  /**
-   * Called after the element's first update. Initializes the state controller.
-   */
-  firstUpdated() {
-    this.state = new MdStateController(this, {
-      button: this.fabNative,
-    });
-
-    // this.state.options.inverted = this.ui === "filled";
-    // this.state.options.size = this.ui ? (40 / 40) * 100 : (40 / 24) * 100;
-    // this.requestUpdate();
-  }
-
-  /**
-   * Called when the element is updated. Handles size and extended property changes.
-   *
-   * @param {Map} _changedProperties - Map of changed properties.
-   */
-  updated(_changedProperties) {
-    if (_changedProperties.has('size')) {
-      ['small', 'large'].forEach((size) => {
-        this.classList.remove('md-fab--' + size);
-      });
-      if (this.size) {
-        this.classList.add('md-fab--' + this.size);
-      }
     }
-    if (_changedProperties.has('extended')) {
-      if (this.extended) {
-        this.classList.add('md-fab--extended');
-      } else {
-        this.classList.remove('md-fab--extended');
-      }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.classList.add("md-fab");
     }
-  }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        this.classList.remove("md-fab");
+    }
+
+    /**
+     * Returns the native button element of the FAB.
+     *
+     * @readonly
+     * @returns {HTMLButtonElement} The native button element.
+     */
+    get fabNative() {
+        return this.querySelector(".md-fab__native");
+    }
+
+    /**
+     * Called after the element's first update. Initializes the state controller.
+     */
+    firstUpdated() {
+        this.state = new MdStateController(this, {
+            button: this.fabNative,
+        });
+
+        // this.state.options.inverted = this.ui === "filled";
+        // this.state.options.size = this.ui ? (40 / 40) * 100 : (40 / 24) * 100;
+        // this.requestUpdate();
+    }
+
+    /**
+     * Called when the element is updated. Handles size and extended property changes.
+     *
+     * @param {Map} _changedProperties - Map of changed properties.
+     */
+    updated(_changedProperties) {
+        if (_changedProperties.has("size")) {
+            ["small", "large"].forEach((size) => {
+                this.classList.remove("md-fab--" + size);
+            });
+            if (this.size) {
+                this.classList.add("md-fab--" + this.size);
+            }
+        }
+        if (_changedProperties.has("extended")) {
+            if (this.extended) {
+                this.classList.add("md-fab--extended");
+            } else {
+                this.classList.remove("md-fab--extended");
+            }
+        }
+    }
 }
 
 /**
@@ -109,6 +109,6 @@ class MdFabComponent extends LitElement {
  * @property {String} detail - Event details.
  */
 
-customElements.define('md-fab', MdFabComponent);
+customElements.define("md-fab", MdFabComponent);
 
 export { MdFabComponent };
