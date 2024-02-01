5fabce9c0e696476cff63d9f69cd53893c93e3f4
Ridho Prasetya <ndiing.inc@gmail.com>
2024-02-02 02:11:22 +0700

    save

--- src/material/material.js
+++ src/material/material.js
@@ -1,10 +1,12 @@
-import "./poper/poper.js";
-import "./ripple/ripple.js";
-import "./router/router.js";
-import "./base/controller.js";
-import "./base/component.js";
+// import "./poper/poper.js";
+// import "./ripple/ripple.js";
+// import "./router/router.js";
+
+// import "./base/controller.js";
+// import "./base/component.js";
 // import "./base/example-controller.js";
 // import "./base/example-component.js";
+
 import "./layout/layout.js";
 import "./icon/icon.js";
 import "./emoji/emoji.js";
