01640aaff825f1018000fe9a98dbae2c75478457
Ridho Prasetya <ndiing.inc@gmail.com>
2024-02-04 09:35:04 +0700

    clean code

--- src/dev/dev.js
+++ src/dev/dev.js
@@ -1,98 +1,48 @@
-import SheetComponent from "./sheet/sheet.js";
-import ImageComponent from "./image/image.js";
-import EmojiComponent from "./emoji/emoji.js";
-import IconComponent from "./icon/icon.js";
-import BottomAppBarComponent from "./bottom-app-bar/bottom-app-bar.js";
-import TopAppBarComponent from "./top-app-bar/top-app-bar.js";
-import BadgeComponent from "./badge/badge.js";
-import ButtonComponent from "./button/button.js";
-import FabComponent from "./fab/fab.js";
-import IconButtonComponent from "./icon-button/icon-button.js";
-import SegmentedButtonComponent from "./segmented-button/segmented-button.js";
-import CardComponent from "./card/card.js";
-import CarouselComponent from "./carousel/carousel.js";
-import CheckboxComponent from "./checkbox/checkbox.js";
-import ChipComponent from "./chip/chip.js";
-import DatePickerComponent from "./date-picker/date-picker.js";
-import DialogComponent from "./dialog/dialog.js";
-import DividerComponent from "./divider/divider.js";
-import ListComponent from "./list/list.js";
-import MenuComponent from "./menu/menu.js";
-import NavigationBarComponent from "./navigation-bar/navigation-bar.js";
-import NavigationDrawerComponent from "./navigation-drawer/navigation-drawer.js";
-import NavigationRailComponent from "./navigation-rail/navigation-rail.js";
-import ProgressIndicatorComponent from "./progress-indicator/progress-indicator.js";
-import RadioButtonComponent from "./radio-button/radio-button.js";
-import SearchComponent from "./search/search.js";
-import BottomSheetComponent from "./bottom-sheet/bottom-sheet.js";
-import SideSheetComponent from "./side-sheet/side-sheet.js";
-import SliderComponent from "./slider/slider.js";
-import SnackbarComponent from "./snackbar/snackbar.js";
-import SwitchComponent from "./switch/switch.js";
-import TabComponent from "./tab/tab.js";
-import TextFieldComponent from "./text-field/text-field.js";
-import TimePickerComponent from "./time-picker/time-picker.js";
 import ErrorComponent from "./error/error.js";
-import LoginComponent from "./login/login.js";
 import MainComponent from "./main/main.js";
-import TooltipComponent from "./tooltip/tooltip.js";
-import UsersComponent from "./users/users.js";
-import UserComponent from "./user/user.js";
-import BlogsComponent from "./blogs/blogs.js";
-import BlogComponent from "./blog/blog.js";
+
 // Usage example
 import { MDRouter } from "../material/router/router";
 // prettier-ignore
 MDRouter.register([
     {
         path: '', component: MainComponent, children: [
-            {
-                path: 'users', load: () => import("./users/users.js").then(m => m.default), children: [
-                    { path: ':id', load: () => import("./user/user.js").then(m => m.default), children: [] },
-                ]
-            },
-            {
-                path: 'blogs', load: () => import("./blogs/blogs.js").then(m => m.default), children: [
-                    { path: ':id', load: () => import("./blog/blog.js").then(m => m.default), outlet: 'blog', children: [] },
-                ]
-            },
-            { path: "badge", title: "badge", component: BadgeComponent },
-            { path: "bottom-app-bar", title: "bottom-app-bar", component: BottomAppBarComponent },
-            { path: "bottom-sheet", title: "bottom-sheet", component: BottomSheetComponent },
-            { path: "button", title: "button", component: ButtonComponent },
-            { path: "card", title: "card", component: CardComponent },
-            { path: "carousel", title: "carousel", component: CarouselComponent },
-            { path: "checkbox", title: "checkbox", component: CheckboxComponent },
-            { path: "chip", title: "chip", component: ChipComponent },
-            { path: "date-picker", title: "date-picker", component: DatePickerComponent },
-            { path: "dialog", title: "dialog", component: DialogComponent },
-            { path: "divider", title: "divider", component: DividerComponent },
-            { path: "emoji", title: "emoji", component: EmojiComponent },
-            { path: "fab", title: "fab", component: FabComponent },
-            { path: "icon", title: "icon", component: IconComponent },
-            { path: "icon-button", title: "icon-button", component: IconButtonComponent },
-            { path: "image", title: "image", component: ImageComponent },
-            { path: "list", title: "list", component: ListComponent },
-            { path: "menu", title: "menu", component: MenuComponent },
-            { path: "navigation-bar", title: "navigation-bar", component: NavigationBarComponent },
-            { path: "navigation-drawer", title: "navigation-drawer", component: NavigationDrawerComponent },
-            { path: "navigation-rail", title: "navigation-rail", component: NavigationRailComponent },
-            { path: "progress-indicator", title: "progress-indicator", component: ProgressIndicatorComponent },
-            { path: "radio-button", title: "radio-button", component: RadioButtonComponent },
-            { path: "search", title: "search", component: SearchComponent },
-            { path: "segmented-button", title: "segmented-button", component: SegmentedButtonComponent },
-            { path: "sheet", title: "sheet", component: SheetComponent },
-            { path: "side-sheet", title: "side-sheet", component: SideSheetComponent },
-            { path: "slider", title: "slider", component: SliderComponent },
-            { path: "snackbar", title: "snackbar", component: SnackbarComponent },
-            { path: "switch", title: "switch", component: SwitchComponent },
-            { path: "tab", title: "tab", component: TabComponent },
-            { path: "text-field", title: "text-field", component: TextFieldComponent },
-            { path: "time-picker", title: "time-picker", component: TimePickerComponent },
-            { path: "tooltip", title: "tooltip", component: TooltipComponent },
-            { path: "top-app-bar", title: "top-app-bar", component: TopAppBarComponent },
+            { path: "badge", title: "badge", load: () => import("./badge/badge.js").then(m=>m.default)},
+            { path: "bottom-app-bar", title: "bottom-app-bar", load: () => import("./bottom-app-bar/bottom-app-bar.js").then(m=>m.default)},
+            { path: "bottom-sheet", title: "bottom-sheet", load: () => import("./bottom-sheet/bottom-sheet.js").then(m=>m.default)},
+            { path: "button", title: "button", load: () => import("./button/button.js").then(m=>m.default)},
+            { path: "card", title: "card", load: () => import("./card/card.js").then(m=>m.default)},
+            { path: "carousel", title: "carousel", load: () => import("./carousel/carousel.js").then(m=>m.default)},
+            { path: "checkbox", title: "checkbox", load: () => import("./checkbox/checkbox.js").then(m=>m.default)},
+            { path: "chip", title: "chip", load: () => import("./chip/chip.js").then(m=>m.default)},
+            { path: "date-picker", title: "date-picker", load: () => import("./date-picker/date-picker.js").then(m=>m.default)},
+            { path: "dialog", title: "dialog", load: () => import("./dialog/dialog.js").then(m=>m.default)},
+            { path: "divider", title: "divider", load: () => import("./divider/divider.js").then(m=>m.default)},
+            { path: "emoji", title: "emoji", load: () => import("./emoji/emoji.js").then(m=>m.default)},
+            { path: "fab", title: "fab", load: () => import("./fab/fab.js").then(m=>m.default)},
+            { path: "icon", title: "icon", load: () => import("./icon/icon.js").then(m=>m.default)},
+            { path: "icon-button", title: "icon-button", load: () => import("./icon-button/icon-button.js").then(m=>m.default)},
+            { path: "image", title: "image", load: () => import("./image/image.js").then(m=>m.default)},
+            { path: "list", title: "list", load: () => import("./list/list.js").then(m=>m.default)},
+            { path: "menu", title: "menu", load: () => import("./menu/menu.js").then(m=>m.default)},
+            { path: "navigation-bar", title: "navigation-bar", load: () => import("./navigation-bar/navigation-bar.js").then(m=>m.default)},
+            { path: "navigation-drawer", title: "navigation-drawer", load: () => import("./navigation-drawer/navigation-drawer.js").then(m=>m.default)},
+            { path: "navigation-rail", title: "navigation-rail", load: () => import("./navigation-rail/navigation-rail.js").then(m=>m.default)},
+            { path: "progress-indicator", title: "progress-indicator", load: () => import("./progress-indicator/progress-indicator.js").then(m=>m.default)},
+            { path: "radio-button", title: "radio-button", load: () => import("./radio-button/radio-button.js").then(m=>m.default)},
+            { path: "search", title: "search", load: () => import("./search/search.js").then(m=>m.default)},
+            { path: "segmented-button", title: "segmented-button", load: () => import("./segmented-button/segmented-button.js").then(m=>m.default)},
+            { path: "sheet", title: "sheet", load: () => import("./sheet/sheet.js").then(m=>m.default)},
+            { path: "side-sheet", title: "side-sheet", load: () => import("./side-sheet/side-sheet.js").then(m=>m.default)},
+            { path: "slider", title: "slider", load: () => import("./slider/slider.js").then(m=>m.default)},
+            { path: "snackbar", title: "snackbar", load: () => import("./snackbar/snackbar.js").then(m=>m.default)},
+            { path: "switch", title: "switch", load: () => import("./switch/switch.js").then(m=>m.default)},
+            { path: "tab", title: "tab", load: () => import("./tab/tab.js").then(m=>m.default)},
+            { path: "text-field", title: "text-field", load: () => import("./text-field/text-field.js").then(m=>m.default)},
+            { path: "time-picker", title: "time-picker", load: () => import("./time-picker/time-picker.js").then(m=>m.default)},
+            { path: "tooltip", title: "tooltip", load: () => import("./tooltip/tooltip.js").then(m=>m.default)},
+            { path: "top-app-bar", title: "top-app-bar", load: () => import("./top-app-bar/top-app-bar.js").then(m=>m.default)},
         ]
     },
-    { path: 'login', component: LoginComponent, children: [] },
     { path: '*', component: ErrorComponent, children: [] },
 ])
