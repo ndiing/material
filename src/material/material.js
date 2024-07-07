a31942f6ae5c03bbc4705d20e25c2e2b0bafc482
Ridho Prasetya <ndiing.inc@gmail.com>
2024-07-07 16:58:42 +0700

    save

--- src/material/material.js
+++ src/material/material.js
@@ -11,7 +11,6 @@ import "./observer/observer.js";
 import "./attribute-observer/attribute-observer.js";
 import "./virtual/virtual.js";
 import "./store/store.js";
-
 import "./markdown/markdown.js";
 import "./icon/icon.js";
 import "./emoji/emoji.js";
@@ -27,7 +26,6 @@ import "./bottom-sheet/bottom-sheet.js";
 import "./segmented-button/segmented-button.js";
 import "./snackbar/snackbar.js";
 import "./tooltip/tooltip.js";
-
 import "./datetime-picker/datetime-picker.js";
 import "./date-picker/date-picker.js";
 import "./month-picker/month-picker.js";
@@ -35,34 +33,27 @@ import "./time-picker/time-picker.js";
 import "./week-picker/week-picker.js";
 import "./color-picker/color-picker.js";
 import "./emoji-picker/emoji-picker.js";
-
 import "./fab/fab.js";
 import "./bottom-app-bar/bottom-app-bar.js";
 import "./image/image.js";
 import "./badge/badge.js";
-
 import "./form/form.js";
-
 import "./checkbox/checkbox.js";
 import "./radio-button/radio-button.js";
 import "./switch/switch.js";
-
 import "./slider/slider.js";
 import "./progress-indicator/progress-indicator.js";
 import "./list-item/list-item.js";
 import "./list/list.js";
 import "./tree-item/tree-item.js";
 import "./tree/tree.js";
-
 import "./navigation-bar/navigation-bar.js";
 import "./navigation-drawer/navigation-drawer.js";
 import "./navigation-rail/navigation-rail.js";
-
 import "./menu/menu.js";
 import "./tabs/tabs.js";
 import "./chip/chip.js";
 import "./chips/chips.js";
-
 import "./text-field/text-field.js";
 import "./datetime-field/datetime-field.js";
 import "./date-field/date-field.js";
@@ -73,7 +64,6 @@ import "./color-field/color-field.js";
 import "./number-field/number-field.js";
 import "./search-field/search-field.js";
 import "./password-field/password-field.js";
-
 import "./pagination/pagination.js";
 import "./data-table-item/data-table-item.js";
 import "./data-table/data-table.js";
