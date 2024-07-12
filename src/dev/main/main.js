768703773fc91087a23c8958d5cb026b6add34a7
Ridho Prasetya <ndiing.inc@gmail.com>
2024-07-12 14:24:02 +0700

    save

--- src/dev/main/main.js
+++ src/dev/main/main.js
@@ -18,7 +18,80 @@ class DevMainComponent extends MDComponent {
     constructor() {
         super();
 
-        this.list = ["functions", "localization", "color", "component", "router", "ripple", "popper", "progress", "gesture", "observer", "attribute-observer", "virtual", "store", "markdown", "icon", "emoji", "image", "badge", "button", "icon-button", "segmented-button", "fab", "card", "scrim", "sheet", "dialog", "top-app-bar", "side-sheet", "bottom-sheet", "snackbar", "tooltip", "bottom-app-bar", "datetime-picker", "date-picker", "month-picker", "time-picker", "week-picker", "color-picker", "emoji-picker", "form", "checkbox", "radio-button", "switch", "slider", "progress-indicator", "list-item", "list", "tree-item", "tree", "navigation-bar", "navigation-drawer", "navigation-rail", "menu", "tabs", "chip", "chips", "text-field", "datetime-field", "date-field", "month-field", "week-field", "time-field", "color-field", "number-field", "search-field", "password-field", "textarea-field", "select-field", "pagination", "data-table-item", "data-table"]
+        this.list = [
+            //
+            "functions",
+            "localization",
+            "color",
+            "component",
+            "router",
+            "ripple",
+            "popper",
+            "progress",
+            "gesture",
+            "observer",
+            "attribute-observer",
+            "virtual",
+            "store",
+            "markdown",
+            "icon",
+            "emoji",
+            "image",
+            "badge",
+            "button",
+            "icon-button",
+            "segmented-button",
+            "fab",
+            "card",
+            "scrim",
+            "sheet",
+            "dialog",
+            "top-app-bar",
+            "side-sheet",
+            "bottom-sheet",
+            "snackbar",
+            "tooltip",
+            "bottom-app-bar",
+            "datetime-picker",
+            "date-picker",
+            "month-picker",
+            "time-picker",
+            "week-picker",
+            "color-picker",
+            "emoji-picker",
+            "form",
+            "checkbox",
+            "radio-button",
+            "switch",
+            "slider",
+            "progress-indicator",
+            // "list-item",
+            "list",
+            // "tree-item",
+            "tree",
+            "navigation-bar",
+            "navigation-drawer",
+            "navigation-rail",
+            "menu",
+            "tabs",
+            "chip",
+            "chips",
+            "text-field",
+            "datetime-field",
+            "date-field",
+            "month-field",
+            "week-field",
+            "time-field",
+            "color-field",
+            "number-field",
+            "search-field",
+            "password-field",
+            "textarea-field",
+            "select-field",
+            "pagination",
+            // "data-table-item",
+            "data-table",
+        ]
             .sort((a, b) => a.localeCompare(b))
             .map((name) => ({
                 routerLink: "/" + name,
@@ -50,13 +123,12 @@ class DevMainComponent extends MDComponent {
                 ></md-navigation-drawer>
                 <div class="md-layout-border__item md-layout-border__item--center">
                     <md-outlet></md-outlet>
-                    
+
                     <div class="md-layout-column">
                         <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium8 md-layout-column__item--compact4">
                             <md-markdown .text="${this.doc}"></md-markdown>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         `;
