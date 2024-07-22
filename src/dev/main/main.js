5355421676f837647137333de00e37bff4aa12e5
Ridho Prasetya <ndiing.inc@gmail.com>
2024-07-22 09:09:45 +0700

    update demo

--- src/dev/main/main.js
+++ src/dev/main/main.js
@@ -16,370 +16,126 @@ class DevMainComponent extends MDComponent {
 
     constructor() {
         super();
-
         this.items = [
             {
                 label: "cdk",
                 items: [
-                    {
-                        label: "observer",
-                        routerLink: "/observer",
-                    },
-                    {
-                        label: "media-observer",
-                        routerLink: "/media-observer",
-                    },
-                    {
-                        label: "attribute-observer",
-                        routerLink: "/attribute-observer",
-                    },
-                    {
-                        label: "store",
-                        routerLink: "/store",
-                    },
-                    {
-                        label: "router",
-                        routerLink: "/router",
-                    },
-                    {
-                        label: "localization",
-                        routerLink: "/localization",
-                    },
-                    {
-                        label: "color",
-                        routerLink: "/color",
-                    },
-                    {
-                        label: "progress",
-                        routerLink: "/progress",
-                    },
-                    {
-                        label: "functions",
-                        routerLink: "/functions",
-                    },
-                    {
-                        label: "ripple",
-                        routerLink: "/ripple",
-                    },
-                    {
-                        label: "popper",
-                        routerLink: "/popper",
-                    },
-                    {
-                        label: "gesture",
-                        routerLink: "/gesture",
-                    },
-                    {
-                        label: "virtual",
-                        routerLink: "/virtual",
-                    },
-                    {
-                        label: "component",
-                        routerLink: "/component",
-                    },
-                    {
-                        label: "template",
-                        routerLink: "/template",
-                    },
+                    { label: "observer", routerLink: "/observer" },
+                    { label: "media-observer", routerLink: "/media-observer" },
+                    { label: "attribute-observer", routerLink: "/attribute-observer" },
+                    { label: "store", routerLink: "/store" },
+                    { label: "router", routerLink: "/router" },
+                    { label: "localization", routerLink: "/localization" },
+                    { label: "color", routerLink: "/color" },
+                    { label: "progress", routerLink: "/progress" },
+                    { label: "functions", routerLink: "/functions" },
+                    { label: "ripple", routerLink: "/ripple" },
+                    { label: "popper", routerLink: "/popper" },
+                    { label: "gesture", routerLink: "/gesture" },
+                    { label: "virtual", routerLink: "/virtual" },
+                    { label: "component", routerLink: "/component" },
+                    { label: "template", routerLink: "/template" },
                 ],
             },
             {
                 label: "base",
                 items: [
-                    {
-                        label: "layout-item",
-                        routerLink: "/layout-item",
-                    },
-                    {
-                        label: "layout",
-                        routerLink: "/layout",
-                    },
-                    {
-                        label: "markdown",
-                        routerLink: "/markdown",
-                    },
-                    {
-                        label: "divider",
-                        routerLink: "/divider",
-                    },
-                    {
-                        label: "spacer",
-                        routerLink: "/spacer",
-                    },
-                    {
-                        label: "icon",
-                        routerLink: "/icon",
-                    },
-                    {
-                        label: "emoji",
-                        routerLink: "/emoji",
-                    },
-                    {
-                        label: "image",
-                        routerLink: "/image",
-                    },
-                    {
-                        label: "badge",
-                        routerLink: "/badge",
-                    },
+                    { label: "layout-item", routerLink: "/layout-item" },
+                    { label: "layout", routerLink: "/layout" },
+                    { label: "markdown", routerLink: "/markdown" },
+                    { label: "divider", routerLink: "/divider" },
+                    { label: "spacer", routerLink: "/spacer" },
+                    { label: "icon", routerLink: "/icon" },
+                    { label: "emoji", routerLink: "/emoji" },
+                    { label: "image", routerLink: "/image" },
+                    { label: "badge", routerLink: "/badge" },
                 ],
             },
             {
                 label: "button",
                 items: [
-                    {
-                        label: "button",
-                        routerLink: "/button",
-                    },
-                    {
-                        label: "icon-button",
-                        routerLink: "/icon-button",
-                    },
-                    {
-                        label: "segmented-button",
-                        routerLink: "/segmented-button",
-                    },
-                    {
-                        label: "fab",
-                        routerLink: "/fab",
-                    },
+                    { label: "button", routerLink: "/button" },
+                    { label: "icon-button", routerLink: "/icon-button" },
+                    { label: "segmented-button", routerLink: "/segmented-button" },
+                    { label: "fab", routerLink: "/fab" },
                 ],
             },
             {
                 label: "form",
                 items: [
-                    {
-                        label: "form",
-                        routerLink: "/form",
-                    },
-                    {
-                        label: "checkbox",
-                        routerLink: "/checkbox",
-                    },
-                    {
-                        label: "radio-button",
-                        routerLink: "/radio-button",
-                    },
-                    {
-                        label: "switch",
-                        routerLink: "/switch",
-                    },
-                    {
-                        label: "slider",
-                        routerLink: "/slider",
-                    },
-                    {
-                        label: "text-field",
-                        routerLink: "/text-field",
-                    },
-                    {
-                        label: "datetime-field",
-                        routerLink: "/datetime-field",
-                    },
-                    {
-                        label: "date-field",
-                        routerLink: "/date-field",
-                    },
-                    {
-                        label: "month-field",
-                        routerLink: "/month-field",
-                    },
-                    {
-                        label: "week-field",
-                        routerLink: "/week-field",
-                    },
-                    {
-                        label: "time-field",
-                        routerLink: "/time-field",
-                    },
-                    {
-                        label: "color-field",
-                        routerLink: "/color-field",
-                    },
-                    {
-                        label: "number-field",
-                        routerLink: "/number-field",
-                    },
-                    {
-                        label: "search-field",
-                        routerLink: "/search-field",
-                    },
-                    {
-                        label: "password-field",
-                        routerLink: "/password-field",
-                    },
-                    {
-                        label: "textarea-field",
-                        routerLink: "/textarea-field",
-                    },
-                    {
-                        label: "select-field",
-                        routerLink: "/select-field",
-                    },
-                    {
-                        label: "progress-indicator",
-                        routerLink: "/progress-indicator",
-                    },
+                    { label: "form", routerLink: "/form" },
+                    { label: "checkbox", routerLink: "/checkbox" },
+                    { label: "radio-button", routerLink: "/radio-button" },
+                    { label: "switch", routerLink: "/switch" },
+                    { label: "slider", routerLink: "/slider" },
+                    { label: "text-field", routerLink: "/text-field" },
+                    { label: "datetime-field", routerLink: "/datetime-field" },
+                    { label: "date-field", routerLink: "/date-field" },
+                    { label: "month-field", routerLink: "/month-field" },
+                    { label: "week-field", routerLink: "/week-field" },
+                    { label: "time-field", routerLink: "/time-field" },
+                    { label: "color-field", routerLink: "/color-field" },
+                    { label: "number-field", routerLink: "/number-field" },
+                    { label: "search-field", routerLink: "/search-field" },
+                    { label: "password-field", routerLink: "/password-field" },
+                    { label: "textarea-field", routerLink: "/textarea-field" },
+                    { label: "select-field", routerLink: "/select-field" },
+                    { label: "progress-indicator", routerLink: "/progress-indicator" },
                 ],
             },
             {
                 label: "card",
                 items: [
-                    {
-                        label: "box",
-                        routerLink: "/box",
-                    },
-                    {
-                        label: "toolbar",
-                        routerLink: "/toolbar",
-                    },
-                    {
-                        label: "card",
-                        routerLink: "/card",
-                    },
-                    {
-                        label: "scrim",
-                        routerLink: "/scrim",
-                    },
-                    {
-                        label: "sheet",
-                        routerLink: "/sheet",
-                    },
-                    {
-                        label: "dialog",
-                        routerLink: "/dialog",
-                    },
-                    {
-                        label: "top-app-bar",
-                        routerLink: "/top-app-bar",
-                    },
-                    {
-                        label: "side-sheet",
-                        routerLink: "/side-sheet",
-                    },
-                    {
-                        label: "bottom-sheet",
-                        routerLink: "/bottom-sheet",
-                    },
-                    {
-                        label: "snackbar",
-                        routerLink: "/snackbar",
-                    },
-                    {
-                        label: "tooltip",
-                        routerLink: "/tooltip",
-                    },
-                    {
-                        label: "bottom-app-bar",
-                        routerLink: "/bottom-app-bar",
-                    },
+                    // { label: "box", routerLink: "/box" },
+                    { label: "toolbar", routerLink: "/toolbar" },
+                    { label: "card", routerLink: "/card" },
+                    { label: "scrim", routerLink: "/scrim" },
+                    { label: "sheet", routerLink: "/sheet" },
+                    { label: "dialog", routerLink: "/dialog" },
+                    { label: "top-app-bar", routerLink: "/top-app-bar" },
+                    { label: "side-sheet", routerLink: "/side-sheet" },
+                    { label: "bottom-sheet", routerLink: "/bottom-sheet" },
+                    { label: "snackbar", routerLink: "/snackbar" },
+                    { label: "tooltip", routerLink: "/tooltip" },
+                    { label: "bottom-app-bar", routerLink: "/bottom-app-bar" },
                 ],
             },
             {
                 label: "picker",
                 items: [
-                    {
-                        label: "datetime-picker",
-                        routerLink: "/datetime-picker",
-                    },
-                    {
-                        label: "date-picker",
-                        routerLink: "/date-picker",
-                    },
-                    {
-                        label: "month-picker",
-                        routerLink: "/month-picker",
-                    },
-                    {
-                        label: "time-picker",
-                        routerLink: "/time-picker",
-                    },
-                    {
-                        label: "week-picker",
-                        routerLink: "/week-picker",
-                    },
-                    {
-                        label: "color-picker",
-                        routerLink: "/color-picker",
-                    },
-                    {
-                        label: "emoji-picker",
-                        routerLink: "/emoji-picker",
-                    },
+                    { label: "datetime-picker", routerLink: "/datetime-picker" },
+                    { label: "date-picker", routerLink: "/date-picker" },
+                    { label: "month-picker", routerLink: "/month-picker" },
+                    { label: "time-picker", routerLink: "/time-picker" },
+                    { label: "week-picker", routerLink: "/week-picker" },
+                    { label: "color-picker", routerLink: "/color-picker" },
+                    { label: "emoji-picker", routerLink: "/emoji-picker" },
                 ],
             },
             {
                 label: "list",
                 items: [
-                    {
-                        label: "list-item",
-                        routerLink: "/list-item",
-                    },
-                    {
-                        label: "list",
-                        routerLink: "/list",
-                    },
-                    {
-                        label: "tree-item",
-                        routerLink: "/tree-item",
-                    },
-                    {
-                        label: "tree",
-                        routerLink: "/tree",
-                    },
-                    {
-                        label: "chip",
-                        routerLink: "/chip",
-                    },
-                    {
-                        label: "chips",
-                        routerLink: "/chips",
-                    },
-                    {
-                        label: "navigation-bar",
-                        routerLink: "/navigation-bar",
-                    },
-                    {
-                        label: "navigation-drawer",
-                        routerLink: "/navigation-drawer",
-                    },
-                    {
-                        label: "navigation-rail",
-                        routerLink: "/navigation-rail",
-                    },
-                    {
-                        label: "menu",
-                        routerLink: "/menu",
-                    },
-                    {
-                        label: "tabs",
-                        routerLink: "/tabs",
-                    },
+                    { label: "list-item", routerLink: "/list-item" },
+                    { label: "list", routerLink: "/list" },
+                    { label: "tree-item", routerLink: "/tree-item" },
+                    { label: "tree", routerLink: "/tree" },
+                    { label: "chip", routerLink: "/chip" },
+                    { label: "chips", routerLink: "/chips" },
+                    { label: "navigation-bar", routerLink: "/navigation-bar" },
+                    { label: "navigation-drawer", routerLink: "/navigation-drawer" },
+                    { label: "navigation-rail", routerLink: "/navigation-rail" },
+                    { label: "menu", routerLink: "/menu" },
+                    { label: "tabs", routerLink: "/tabs" },
                 ],
             },
             {
                 label: "table",
                 items: [
-                    {
-                        label: "pagination",
-                        routerLink: "/pagination",
-                    },
-                    {
-                        label: "data-table-item",
-                        routerLink: "/data-table-item",
-                    },
-                    {
-                        label: "data-table-column-cell",
-                        routerLink: "/data-table-column-cell",
-                    },
-                    {
-                        label: "data-table-row-cell",
-                        routerLink: "/data-table-row-cell",
-                    },
-                    {
-                        label: "data-table",
-                        routerLink: "/data-table",
-                    },
+                    { label: "pagination", routerLink: "/pagination" },
+                    { label: "data-table-item", routerLink: "/data-table-item" },
+                    { label: "data-table-column-cell", routerLink: "/data-table-column-cell" },
+                    { label: "data-table-row-cell", routerLink: "/data-table-row-cell" },
+                    { label: "data-table", routerLink: "/data-table" },
                 ],
             },
         ];
