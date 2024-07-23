0ae5b831ca761d089d889db23ce3a2080d9ab60e
Ridho Prasetya <ndiing.inc@gmail.com>
2024-07-23 13:42:48 +0700

    save

--- src/dev/dialog/dialog.js
+++ src/dev/dialog/dialog.js
@@ -28,7 +28,8 @@ const actions1 = [
     },
 ];
 const actions2 = [{ component: "button", label: "save" }];
-const actions3 = [{ component: "spacer" },
+const actions3 = [
+    { component: "spacer" },
     {
         label: "label",
         onButtonClick: () => {
@@ -40,7 +41,8 @@ const actions3 = [{ component: "spacer" },
         onButtonClick: () => {
             dialog2.close();
         },
-    },];
+    },
+];
 
 class DevDialog extends MDComponent {
     render() {
@@ -48,7 +50,6 @@ class DevDialog extends MDComponent {
             <div class="md-layout-border">
                 <div class="md-layout-border__item md-layout-border__item--center">
                     <div class="md-layout-column">
-
                         <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium8 md-layout-column__item--compact4">
                             <md-dialog id="dialog0" style="width:280px;" headline="Lorem ipsum dolor" .actions="${actions0}"> Lorem ipsum dolor sit amet. Beatae commodi eos eligendi est? Illo tempora a rerum ex. </md-dialog>
                             <md-button label="show dialog" variant="tonal" @click="${() => dialog0.show(true)}"></md-button>
@@ -58,12 +59,11 @@ class DevDialog extends MDComponent {
                             <md-dialog id="dialog1" variant="full" headline="Lorem ipsum dolor" .leadingActions="${actions1}" .trailingActions="${actions2}"> Lorem ipsum dolor sit amet. Beatae commodi eos eligendi est? Illo tempora a rerum ex. </md-dialog>
                             <md-button label="show dialog" variant="tonal" @click="${() => dialog1.show(true)}"></md-button>
                         </div>
-                        
+
                         <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium8 md-layout-column__item--compact4">
                             <md-dialog id="dialog2" style="width:280px;" leadingIcon="image" headline="Lorem ipsum dolor" .actions="${actions3}"> Lorem ipsum dolor sit amet. Beatae commodi eos eligendi est? Illo tempora a rerum ex. </md-dialog>
                             <md-button label="show dialog" variant="tonal" @click="${() => dialog2.show(true)}"></md-button>
                         </div>
-
                     </div>
                 </div>
             </div>
