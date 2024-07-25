a095ad1e200d5fd6e00decca791979b709eeb6f5
Ridho Prasetya <ndiing.inc@gmail.com>
2024-07-25 16:25:13 +0700

    save

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    // // Add you postcss configuration here
-    // // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
-    // plugins: [["autoprefixer"]],
+    plugins: [
+        require('postcss-import'),
+        require('autoprefixer'),
+        require('postcss-nested'),
+        require('postcss-preset-env')({
+            stage: 1,
+            features: {
+                'custom-properties': true
+            }
+        }),
+        ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : [])
+    ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const glob = require("glob");
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-const stylesHandler = MiniCssExtractPlugin.loader;
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader";
 
 const config = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "demo"),
+        filename: isProduction ? "[name].[contenthash].js" : "[name].js",
         clean: true,
     },
     devServer: {
@@ -25,54 +28,55 @@ const config = {
         new HtmlWebpackPlugin({
             template: "index.html",
         }),
-
         new MiniCssExtractPlugin({
+            filename: isProduction ? "[name].[contenthash].css" : "[name].css",
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        new PurgeCSSPlugin({
+            paths: glob.sync("./src/**/*.{js,jsx}", { nodir: true }),
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    stylesHandler,
-                    "css-loader",
-                    'postcss-loader',
-                    "sass-loader",
-                ],
+                use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
             },
             {
                 test: /\.css$/i,
-                use: [
-                    stylesHandler,
-                    "css-loader",
-                    'postcss-loader'
-                ],
+                use: [stylesHandler, "css-loader", "postcss-loader"],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: "asset",
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     optimization: {
-        minimize: true, // Ensure JS files are minified
+        splitChunks: {
+            chunks: "all",
+        },
+        runtimeChunk: "single",
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ],
     },
+    cache: {
+        type: "filesystem",
+    },
+    devtool: true ? "source-map" : "eval-cheap-module-source-map",
 };
 
 module.exports = () => {
     if (isProduction) {
         config.mode = "production";
-
         config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
     } else {
         config.mode = "development";
