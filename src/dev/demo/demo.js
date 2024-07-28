6272dd1f6d281cd852890c4cf3448af6ee723d9e
ndiing <ndiing.inc@gmail.com>
2024-07-28 10:33:47 +0700

    temp fix

--- src/dev/demo/demo.js
+++ src/dev/demo/demo.js
@@ -5,13 +5,15 @@ class DevDemo extends MDComponent {
     render() {
         return html`
             <md-layout variant="column">
+                <!--  -->
                 <md-layout-item expanded="6" medium="8" compact="4">
-                    <md-layout variant="border" class="dev-demo__viewport">
-                        <md-layout-item region="center" class="dev-demo__container">
+                    <md-layout variant="border" class="demo">
+                        <md-layout-item region="center" class="demo__container">
                             demo
                         </md-layout-item>
                     </md-layout>
                 </md-layout-item>
+                <!--  -->
             </md-layout>
         `;
     }
