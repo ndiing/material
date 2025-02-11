b8567f8519a5c33a805f3877aed8eb8efe54c7f3
Ridho Prasetya <ndiing.inc@gmail.com>
2025-02-11 23:00:58 +0700

    update demo

--- src/demo/image/image.js
+++ src/demo/image/image.js
@@ -6,14 +6,100 @@ class DemoImage extends MdComponent {
         return html`
             <div class="md-layout">
                 <div class="md-layout__grid">
-                    <div class="md-layout__column--expanded12 md-layout__column--medium4 md-layout__column--compact4">
+
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
                         <md-image
                             src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
-                            alt=""
-                            ratio=""
-                            circular=""
                         ></md-image>
                     </div>
+
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="16/9"
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="3/2"
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="4/3"
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="1/1"
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="3/4"
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="2/3"
+                        ></md-image>
+                    </div>
+
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            circular
+                        ></md-image>
+                    </div>
+
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="16/9"
+                            circular
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="3/2"
+                            circular
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="4/3"
+                            circular
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="1/1"
+                            circular
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="3/4"
+                            circular
+                        ></md-image>
+                    </div>
+                    <div class="md-layout__column--expanded2 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-image
+                            src="https://api.dicebear.com/9.x/dylan/svg?seed=Leah"
+                            ratio="2/3"
+                            circular
+                        ></md-image>
+                    </div>
+
                 </div>
             </div>
         `;
