ef67ddf200174ae9a7dbed5495d20ace9227bcd9
Ridho Prasetya <ndiing.inc@gmail.com>
2025-02-13 23:24:57 +0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
-
 const isProduction = process.env.NODE_ENV === "production";
-
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
     cache: {
-        type: "filesystem", // Simpan hasil build di disk
-        buildDependencies: {
-            config: [__filename], // Menggunakan file konfigurasi sebagai dependency cache
-        },
+        type: "filesystem",
+        buildDependencies: { config: [__filename] },
     },
     optimization: {
-        usedExports: true, // Mengaktifkan tree shaking untuk ekspor yang digunakan
-        splitChunks: {
-            chunks: "all", // Membagi kode untuk semua tipe chunk (async dan non-async)
-        },
+        usedExports: true,
+        splitChunks: { chunks: "all" },
     },
     entry: "./src/index.js",
     output: {
@@ -35,53 +27,14 @@ const config = {
         static: "./",
         hot: true,
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-
-        new MiniCssExtractPlugin(),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" }), new MiniCssExtractPlugin()],
     module: {
         rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                // loader: "babel-loader",
-                exclude: /node_modules/,
-                use: [
-                    "thread-loader", // Tambahkan ini untuk paralelisasi
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            cacheDirectory: true,
-                            // parallel: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, "css-loader", "postcss-loader"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: "asset",
-            },
-
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+            { test: /\.(js|jsx)$/i, exclude: /node_modules/, use: ["thread-loader", { loader: "babel-loader", options: { cacheDirectory: true } }] },
+            { test: /\.s[ac]ss$/i, use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"] },
+            { test: /\.css$/i, use: [stylesHandler, "css-loader", "postcss-loader"] },
+            { test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i, type: "asset" },
+            { test: /\.html$/i, use: ["html-loader"] },
         ],
     },
 };
@@ -89,7 +42,6 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = "production";
-
         config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
     } else {
         config.mode = "development";
