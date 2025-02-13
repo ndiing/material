16ea730692acf0ab8316c4b8987c7c1d604b080f
Ridho Prasetya <ndiing.inc@gmail.com>
2025-02-14 00:54:03 +0700

    update demo

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,7 +1,11 @@
 import { Router } from "../material/router/router";
 import { setTheme } from "../material/color/color.js";
 
-setTheme("#a47764");
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+setTheme(getRandomHexColor());
 
 import DemoMain from "./main/main.js";
 import DemoError from "./error/error.js";
