11426568eab63cd85d6066d43dd66a203c2c733d
Ridho Prasetya <ndiing.inc@gmail.com>
2025-02-15 10:57:28 +0700

    update demo for theming and progress

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import "./docs/docs.scss";
 import "./material/material.js";
 import { Router } from "./material/router/router";
 
+import { Progress } from "./material/progress/progress.js";
+import { setTheme } from "./material/color/color.js";
+
 import DemoRoutes from "./demo/demo.js";
 import DocsRoutes from "./docs/docs.js";
 
@@ -21,33 +24,30 @@ const routes = [
 ];
 Router.use(routes);
 
-// import { Progress } from "./material/progress/progress.js";
-// import { setTheme } from "./material/color/color.js";
+const progress = new Progress();
 
-// const progress = new Progress();
+const fetch = window.fetch
+window.fetch = async function(){
+    performance.mark("mark-fetch-1");
+    const res= await fetch(arguments)
+    performance.mark("mark-fetch-2");
+    performance.measure("measure-fetch-1", "mark-fetch-1", "mark-fetch-2");
+    performance.clearMarks("mark-fetch-1");
+    performance.clearMarks("mark-fetch-2");
+    performance.clearMeasures("measure-fetch-1");
+    return res
+}
 
-// const fetch = window.fetch
-// window.fetch = async function(){
-//     performance.mark("mark-fetch-1");
-//     const res= await fetch(arguments)
-//     performance.mark("mark-fetch-2");
-//     performance.measure("measure-fetch-1", "mark-fetch-1", "mark-fetch-2");
-//     performance.clearMarks("mark-fetch-1");
-//     performance.clearMarks("mark-fetch-2");
-//     performance.clearMeasures("measure-fetch-1");
-//     return res
-// }
+const observer = new PerformanceObserver((entries) => {
+    entries.getEntries().forEach((entry) => progress.start(entry.duration));
+});
+observer.observe({
+    entryTypes: PerformanceObserver.supportedEntryTypes,
+});
 
-// const observer = new PerformanceObserver((entries) => {
-//     entries.getEntries().forEach((entry) => progress.start(entry.duration));
-// });
-// observer.observe({
-//     entryTypes: PerformanceObserver.supportedEntryTypes,
-// });
-
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215)
-//         .toString(16)
-//         .padStart(6, "0")}`;
-// }
-// setTheme(getRandomHexColor());
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, "0")}`;
+}
+setTheme(getRandomHexColor());
