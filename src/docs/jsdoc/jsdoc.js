2a2826ba84f65115e79612d66f3b07790cfa3e74
Ridho Prasetya <ndiing.inc@gmail.com>
2025-02-22 04:57:27 +0700

    Perbarui dokumen dan demo

--- src/docs/jsdoc/jsdoc.js
+++ src/docs/jsdoc/jsdoc.js
@@ -1,15 +1,16 @@
 import jsdoc from "./jsdoc.json";
 
-jsdoc = jsdoc.filter((item) => !item.undocumented);
+const entries = Object.groupBy(
+    jsdoc.filter((item) => !item.undocumented),
+    (item) => item.meta?.filename,
+);
 
-jsdoc = Object.groupBy(jsdoc, (item) => item.meta?.filename);
-
-const keys = Object.keys(jsdoc).map((key) => ({
+const keys = Object.keys(entries).map((key) => ({
     label: key,
     routerLink: "/docs/" + key,
 }));
 
-const values = Object.entries(jsdoc).map(([key, value]) => {
+const values = Object.entries(entries).map(([key, value]) => {
     value = Object.groupBy(value, (item) => item.kind);
     value.key = key;
     return value;
