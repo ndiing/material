73cc5afba2861b933e2f533cc5dce0396168f790
Ridho Prasetya <ndiing.inc@gmail.com>
2025-02-22 09:17:33 +0700

    Code grouping

--- src/material/material.js
+++ src/material/material.js
@@ -1,4 +1,5 @@
 import "./polyfill/polyfill.js";
+
 // import "./util/util.js";
 // import "./localization/localization.js";
 // import "./color/color.js";
@@ -12,6 +13,7 @@ import "./polyfill/polyfill.js";
 // import "./component/component.js";
 // import "./progress/progress.js";
 // import "./router/router.js";
+
 import "./icon/icon.js";
 import "./image/image.js";
 import "./divider/divider.js";
@@ -20,6 +22,7 @@ import "./button/button.js";
 import "./fab/fab.js";
 import "./icon-button/icon-button.js";
 import "./segmented-button/segmented-button.js";
+
 import "./chips/chip.js";
 import "./chips/chips.js";
 import "./list/list-row.js";
@@ -30,34 +33,45 @@ import "./tree/tree-item.js";
 import "./tree/tree.js";
 import "./tabs/tab.js";
 import "./tabs/tabs.js";
+
 import "./card/card.js";
 import "./scrim/scrim.js";
+
 import "./dialog/dialog.js";
 import "./snackbar/snackbar.js";
 import "./tooltip/tooltip.js";
+
 import "./sheet/sheet.js";
 import "./bottom-sheet/bottom-sheet.js";
 import "./side-sheet/side-sheet.js";
 import "./bottom-app-bar/bottom-app-bar.js";
 import "./top-app-bar/top-app-bar.js";
+
 import "./navigation-bar/navigation-bar.js";
 import "./navigation-drawer/navigation-drawer.js";
 import "./navigation-rail/navigation-rail.js";
 import "./menu/menu.js";
+
 import "./datetime-picker/datetime-picker.js";
 import "./date-picker/date-picker.js";
 import "./time-picker/time-picker.js";
 import "./month-picker/month-picker.js";
 import "./week-picker/week-picker.js";
+
 import "./data-table/data-table-native.js";
 import "./data-table/data-table-cell.js";
 import "./data-table/data-table.js";
+
 import "./progress-indicator/progress-indicator.js";
+
 import "./form/form.js";
+
 import "./checkbox/checkbox.js";
 import "./radio-button/radio-button.js";
 import "./switch/switch.js";
+
 import "./slider/slider.js";
+
 import "./text-field/text-field.js";
 import "./password-field/password-field.js";
 import "./number-field/number-field.js";
