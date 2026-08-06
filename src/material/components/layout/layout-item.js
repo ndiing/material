aa7c4a4c572a39ec08d926963e4714068112934f
Ridho Prasetya <ndiing.inc@gmail.com>
2026-08-06 16:10:18 +0700

    fix(component): perbaikan di layout-item pemisahan state update dan updated penanganan scrim auto show and close

--- src/material/components/layout/layout-item.js
+++ src/material/components/layout/layout-item.js
@@ -4,21 +4,27 @@ class MdLayoutItem extends MdElement {
     static properties = {
         region: { type: String },
         modal: { type: Boolean },
-        open: { type: Boolean },
+        open: { type: Boolean,reflect:true },
     };
+
     regions = ["center", "west", "north", "east", "south"];
 
     constructor() {
         super();
+
         this.region = "center";
+
         this._handleLayoutItemTransitionend = this._handleLayoutItemTransitionend.bind(this);
         this._handleLayoutItemScrimClose = this._handleLayoutItemScrimClose.bind(this);
     }
 
     connectedCallback() {
         super.connectedCallback();
+
         this.classList.add("md-layout__item");
+
         this.on("transitionend", this._handleLayoutItemTransitionend);
+        
         if (!this.scrimElement) {
             this.scrimElement = document.createElement("md-scrim");
             this.parentElement.insertBefore(this.scrimElement, this.nextElementSibling);
@@ -28,24 +34,41 @@ class MdLayoutItem extends MdElement {
 
     disconnectedCallback() {
         super.disconnectedCallback();
+
         if (this.scrimElement) {
             this.scrimElement.off("onScrimClose", this._handleLayoutItemScrimClose);
             this.scrimElement.remove();
             this.scrimElement = null;
         }
+
         this.off("transitionend", this._handleLayoutItemTransitionend);
+
         this.classList.remove("md-layout__item");
     }
 
-    updated(_changedProperties) {
-        if (_changedProperties.has("region")) {
-            this._toggleClassList(this.regions, this.region);
+    update(changedProperties) {
+        super.update(changedProperties)
+
+        if (changedProperties.has("region")) {
+            this.regions.forEach((region) => {
+                this.classList.toggle(`md-layout__item--${region}`, this.region === region);
+            });
         }
-        if (_changedProperties.has("modal")) {
-            this._toggleClass("modal");
+
+        if (changedProperties.has("modal")) {
+            this.classList.toggle(`md-layout__item--modal`, !!this.modal);
         }
-        if (_changedProperties.has("open")) {
-            this._toggleClass("open");
+
+        if (changedProperties.has("open")) {
+            this.classList.toggle(`md-layout__item--open`, !!this.open);
+        }
+    }
+
+    updated(_changedProperties){
+        super.updated(_changedProperties)
+
+        if (changedProperties.has("open")) {
+
             if (this.modal) {
                 if (this.open) {
                     this.scrimElement.show();
@@ -54,16 +77,6 @@ class MdLayoutItem extends MdElement {
                 }
             }
         }
-    }
-
-    _toggleClass(modifier) {
-        this.classList.toggle(`md-layout__item--${modifier}`, !!this[modifier]);
-    }
-
-    _toggleClassList(list, value) {
-        list.forEach((item) => {
-            this.classList.toggle(`md-layout__item--${item}`, value === item);
-        });
     }
 
     _handleLayoutItemTransitionend(event) {
@@ -80,11 +93,13 @@ class MdLayoutItem extends MdElement {
 
     show() {
         this.open = true;
+
         this.emit("onLayoutItemShow", { element: this });
     }
 
     close() {
         this.open = false;
+
         this.emit("onLayoutItemClose", { element: this });
     }
 
