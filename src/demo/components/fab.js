e5553138bfcf36ada9aae5696f924cc589e018f6
Ridho Prasetya <ndiing.inc@gmail.com>
2026-08-11 18:54:40 +0700

    chore(demo): update fab demo, also update supproting css var on fab

--- src/demo/components/fab.js
+++ src/demo/components/fab.js
@@ -5,105 +5,134 @@ class DemoFab extends MdElement {
     /* prettier-ignore */
     render(){
         return html`
-            <md-grid>
+            <md-grid style="margin:24px;">
                 
-                <md-grid-column>
-                    <md-fab icon="edit" size="small"></md-fab>
-                    <md-fab icon="edit" size="medium"></md-fab>
-                    <md-fab icon="edit" size="large"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab icon="edit"></md-fab>
                 </md-grid-column>
-                <md-grid-column>
-                    <md-fab icon="edit" color="primary-container"></md-fab>
-                    <md-fab icon="edit" color="secondary-container"></md-fab>
-                    <md-fab icon="edit" color="tertiary-container"></md-fab>
-                    <md-fab icon="edit" color="primary"></md-fab>
-                    <md-fab icon="edit" color="secondary"></md-fab>
-                    <md-fab icon="edit" color="tertiary"></md-fab>
-                </md-grid-column>
-                <md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab icon="edit" unelevated></md-fab>
                 </md-grid-column>
                 
-
-                <md-grid-column>
-                    <md-fab unelevated icon="edit" size="small"></md-fab>
-                    <md-fab unelevated icon="edit" size="medium"></md-fab>
-                    <md-fab unelevated icon="edit" size="large"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="small" icon="edit"></md-fab>
                 </md-grid-column>
-                <md-grid-column>
-                    <md-fab unelevated icon="edit" color="primary-container"></md-fab>
-                    <md-fab unelevated icon="edit" color="secondary-container"></md-fab>
-                    <md-fab unelevated icon="edit" color="tertiary-container"></md-fab>
-                    <md-fab unelevated icon="edit" color="primary"></md-fab>
-                    <md-fab unelevated icon="edit" color="secondary"></md-fab>
-                    <md-fab unelevated icon="edit" color="tertiary"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="small" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-                <md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="medium" icon="edit"></md-fab>
                 </md-grid-column>
-
-                <md-grid-column>
-                    <md-fab label="Compose" icon="edit" size="small"></md-fab>
-                    <md-fab label="Compose" icon="edit" size="medium"></md-fab>
-                    <md-fab label="Compose" icon="edit" size="large"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="medium" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-                <md-grid-column>
-                    <md-fab label="Compose" icon="edit" color="primary-container"></md-fab>
-                    <md-fab label="Compose" icon="edit" color="secondary-container"></md-fab>
-                    <md-fab label="Compose" icon="edit" color="tertiary-container"></md-fab>
-                    <md-fab label="Compose" icon="edit" color="primary"></md-fab>
-                    <md-fab label="Compose" icon="edit" color="secondary"></md-fab>
-                    <md-fab label="Compose" icon="edit" color="tertiary"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="large" icon="edit"></md-fab>
                 </md-grid-column>
-                <md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="large" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-
-                <md-grid-column>
-                    <md-fab label="Compose" size="small"></md-fab>
-                    <md-fab label="Compose" size="medium"></md-fab>
-                    <md-fab label="Compose" size="large"></md-fab>
+                
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary-container" icon="edit"></md-fab>
                 </md-grid-column>
-                <md-grid-column>
-                    <md-fab label="Compose" color="primary-container"></md-fab>
-                    <md-fab label="Compose" color="secondary-container"></md-fab>
-                    <md-fab label="Compose" color="tertiary-container"></md-fab>
-                    <md-fab label="Compose" color="primary"></md-fab>
-                    <md-fab label="Compose" color="secondary"></md-fab>
-                    <md-fab label="Compose" color="tertiary"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary-container" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-                <md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary-container" icon="edit"></md-fab>
                 </md-grid-column>
-
-                <md-grid-column>
-                    <md-fab unelevated label="Compose" icon="edit" size="small"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" size="medium"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" size="large"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary-container" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-                <md-grid-column>
-                    <md-fab unelevated label="Compose" icon="edit" color="primary-container"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" color="secondary-container"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" color="tertiary-container"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" color="primary"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" color="secondary"></md-fab>
-                    <md-fab unelevated label="Compose" icon="edit" color="tertiary"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary-container" icon="edit"></md-fab>
                 </md-grid-column>
-                <md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary-container" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-
-                <md-grid-column>
-                    <md-fab unelevated label="Compose" size="small"></md-fab>
-                    <md-fab unelevated label="Compose" size="medium"></md-fab>
-                    <md-fab unelevated label="Compose" size="large"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary" icon="edit"></md-fab>
                 </md-grid-column>
-                <md-grid-column>
-                    <md-fab unelevated label="Compose" color="primary-container"></md-fab>
-                    <md-fab unelevated label="Compose" color="secondary-container"></md-fab>
-                    <md-fab unelevated label="Compose" color="tertiary-container"></md-fab>
-                    <md-fab unelevated label="Compose" color="primary"></md-fab>
-                    <md-fab unelevated label="Compose" color="secondary"></md-fab>
-                    <md-fab unelevated label="Compose" color="tertiary"></md-fab>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary" icon="edit" unelevated></md-fab>
                 </md-grid-column>
-                <md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary" icon="edit"></md-fab>
                 </md-grid-column>
-
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary" icon="edit" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary" icon="edit"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary" icon="edit" unelevated></md-fab>
+                </md-grid-column>
+                
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab icon="edit" label="Label"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab icon="edit" label="Label" unelevated></md-fab>
+                </md-grid-column>
+                
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="small" icon="edit" label="Small"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="small" icon="edit" label="Small" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="medium" icon="edit" label="Medium"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="medium" icon="edit" label="Medium" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="large" icon="edit" label="Large"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab size="large" icon="edit" label="Large" unelevated></md-fab>
+                </md-grid-column>
+                
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary-container" icon="edit" label="Primary Container"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary-container" icon="edit" label="Primary Container" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary-container" icon="edit" label="Secondary Container"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary-container" icon="edit" label="Secondary Container" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary-container" icon="edit" label="Tertiary Container"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary-container" icon="edit" label="Tertiary Container" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary" icon="edit" label="Primary"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="primary" icon="edit" label="Primary" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary" icon="edit" label="Secondary"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="secondary" icon="edit" label="Secondary" unelevated></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary" icon="edit" label="Tertiary"></md-fab>
+                </md-grid-column>
+                <md-grid-column expanded="6" medium="4" compact="4">
+                    <md-fab color="tertiary" icon="edit" label="Tertiary" unelevated></md-fab>
+                </md-grid-column>
+                
             </md-grid>
         `
     }
